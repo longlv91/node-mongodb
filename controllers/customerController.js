8b1782cef9d88a24494631f81b24a07fe19c32ee
longlv91 <levanlong.91gmail.com>
2018-06-25 23:21:08 +0700

    Update code

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -1,14 +1,14 @@
 const Customer = require('../models/customerModel');
 
 //Simple version, without validation or sanitation
-exports.list = function (req, res) {
+exports.list = function (req, res, next) {
 	Customer.find({}, function(err, customers) {
 		if (err) return next(err);
 		res.send(customers);
 	});
 };
 
-exports.create = function (req, res) {
+exports.create = function (req, res, next) {
     let customer = new Customer(
         {
             name: req.body.name,
@@ -18,8 +18,12 @@ exports.create = function (req, res) {
 
     customer.save(function (err) {
         if (err) {
+            res.status(500).json({
+                message: "Customer name is existed. Please try again."
+            });
             return next(err);
         }
-        res.send('Customer Created successfully')
+        res.send('Customer Created successfully');
+        res.end();
     })
 };
--- models/customerModel.js
+++ models/customerModel.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const customerModel = new Schema({
-    name: { type: String, required: true},
+    name: { type: String, required: true, unique: true},
     address: { type: String, required: true}
-})
+});
 // Export the model
-module.exports = mongoose.model('customers', customerModel)
+module.exports = mongoose.model('customers', customerModel);
